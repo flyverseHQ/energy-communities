d02306d6cbd1e829f780c6ac0c78ed92f5cec3c4
fesit <wi24b079@technikum-wien.at>
2026-06-15 19:40:38 +0200

    Add community energy producer service

---
+++ community-producer/src/main/java/at/fhtw/disys/producer/CommunityProducerApplication.java
@@ -0,0 +1,12 @@
+package at.fhtw.disys.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityProducerApplication.class, args);
+    }
+}
